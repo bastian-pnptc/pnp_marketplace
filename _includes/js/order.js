48f672e357c057e9b25c1e89e6f13edaf77ff82b
Bastian Kroggel <b.kroggel@yahoo.de>
2018-11-06 10:27:05 +0100

    fixed bug on  categorie value

--- _includes/js/order.js
+++ _includes/js/order.js
@@ -136,7 +136,7 @@ function get_product_info(product) {
   var description = product.data('product_description');
   var image = product.data('product_image');
   var stock = product.data('product_stock');
-  var stock = product.data('product_categorie');
+  var categorie = product.data('product_categorie');
   var amount = product.find('.input_amount input').val();
   var result = product.find('.result .output').text();
   var product_details = [];
