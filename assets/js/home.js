9cbfdb3ba36a38b845c505e37f3774f031ac5adb
Bastian Kroggel <b.kroggel@yahoo.de>
2019-04-11 18:28:53 +0200

    added warning message to let user know this is not a finished product and ready to order with

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,12 @@
 ---
 ---
+$(document).ready(function () {
+    $('body').addClass('deactivated');
+    $('#warning-message button').click( function() {
+        $('body').removeClass('deactivated');
+    })
+});
+
 {%- include js/shave.min.js -%}
 {%- include js/order.js -%}
 {%- include js/cardgrid.js -%}
